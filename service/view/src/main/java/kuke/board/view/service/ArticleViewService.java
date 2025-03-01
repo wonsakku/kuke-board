6077397918c7eb850eb3b548a85b365f636f4e24
wonsakku <kws1044@gmail.com>
2025-03-01 19:41:18 +0900

    조회수 어뷰징 방지 정책 구현

--- service/view/src/main/java/kuke/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/kuke/board/view/service/ArticleViewService.java
@@ -1,6 +1,7 @@
 package kuke.board.view.service;
 
 import kuke.board.view.repository.ArticleViewCountRepository;
+import kuke.board.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +12,17 @@ import java.time.Duration;
 public class ArticleViewService {
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
+
 
     private static final int BACK_UP_BACH_SIZE = 100;
     private static final Duration TTL = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACK_UP_BACH_SIZE == 0) {
             articleViewCountBackUpProcessor.backUp(articleId, count);
