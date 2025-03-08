a17b4406a7a56313277a60368eb9ad0f707eeb7a
wonsakku <kws1044@gmail.com>
2025-03-08 16:28:37 +0900

    인기글 Producuer@Consumer 테스트

---
+++ service/hot-article/src/test/java/kuke/board/hotarticle/api/HotArticleApiTest.java
@@ -0,0 +1,25 @@
+package kuke.board.hotarticle.api;
+
+import kuke.board.hotarticle.service.response.HotArticleResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+public class HotArticleApiTest {
+    RestClient restClient = RestClient.create("http://localhost:9004");
+
+    @Test
+    void readAllTest() {
+        List<HotArticleResponse> responses = restClient.get()
+                .uri("/v1/hot-articles/articles/date/{dateStr}", "20250308")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<HotArticleResponse>>() {
+                });
+
+        for (HotArticleResponse response : responses) {
+            System.out.println("response = " + response);
+        }
+    }
+}
